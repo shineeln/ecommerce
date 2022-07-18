22373983e0171ce06d6dd058fd85c44bfdd826dd
shineeln <enhbaatar13@gmail.com>
2022-07-18 18:15:11 +0800

    api

---
+++ djackets_django/product/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+# from .views import LatestProductsList
+from product import views
+
+urlpatterns = [
+    path('latest-products/', views.LatestProductsList.as_view()),
+]
---
+++ djackets_vue/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Djacket
+        </p>
+        <p class="subtitle">
+          The best jacket store online
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+  },
+  name: 'Home',
+  components: {
+  }
+}
+</script>
