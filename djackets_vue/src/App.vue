e344c7efdd951a799e74ba8b84c192349408e6cf
shineeln <enhbaatar13@gmail.com>
2022-07-21 20:34:52 +0800

    cart

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -14,6 +14,25 @@
       </div>
 
       <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active' : showMobileMenu}">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form action="/search" method="get">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" name="query" id="" class="input" placeholder="What are you looking for?">
+                </div>
+
+                <div class="control">
+                  <button class="button is-success">
+                    <span class="icon">
+                      <i class="fas fa-search"></i>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
@@ -33,6 +52,9 @@
       </div>
     </nav>
 
+    <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading}">
+      <div class="lds-dual-ring"></div>
+    </div>
     <section class="section">
       <router-view></router-view>
     </section>
@@ -75,4 +97,40 @@ export default {
 
 <style lang="scss">
 @import '../node_modules/bulma';
+
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-color: #ccc transparent #ccc transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.is-loading-bar {
+  height: 0;
+  overflow: hidden;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+
+  &.is-loading {
+    height: 80px;
+  }
+}
 </style>
--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '@/views/Search.vue'
+import Cart from '@/views/Cart.vue'
 
 const routes = [
   {
@@ -18,9 +21,24 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path : '/search',
+    name : 'Search',
+    component : Search
+  },
+  {
+    path : '/cart',
+    name : 'Cart',
+    component : Cart
+  },
+  {
     path : '/:category_slug/:product_slug/',
     name : 'Product',
     component : Product
+  },
+  {
+    path : '/:category_slug/',
+    name : 'Category',
+    component : Category
   }
 ]
 
